9d31c16931d0cb106d6a2e55857c0fbef780380d
郑逢时 <962907278@qq.com>
2019-04-06 12:45:01 +0800

    [update]

--- src/camera/BasicCamera.cpp
+++ src/camera/BasicCamera.cpp
@@ -9,11 +9,13 @@ BasicCamera::BasicCamera(const Pos &pos_, const ElAg &euler_angles_, unsigned in
 {
 }
 
+#define SIGMA 0.5
+
 Ray BasicCamera::shootRayAt(unsigned int i, unsigned int j) const
 {
 	using Funcs::randfNormal;
 	// convert screen pixel crd sys (i, j) to screen center crd sys (x', y', z')
-	double xs = (i - w_2 + randfNormal(0, 0.5)) * PIXEL_SIZE, ys = (j - h_2 + randfNormal(0, 0.5)) * PIXEL_SIZE;
+	double xs = (i - w_2 + randfNormal(0, SIGMA)) * PIXEL_SIZE, ys = (j - h_2 + randfNormal(0, SIGMA)) * PIXEL_SIZE;
 //	double xs = (i - w_2) * PIXEL_SIZE, ys = (j - h_2) * PIXEL_SIZE;
 
 	// from screen center crd (x', y') get global crd of ray.dir
@@ -23,3 +25,5 @@ Ray BasicCamera::shootRayAt(unsigned int i, unsigned int j) const
 
 	return {pos, dir};
 }
+
+#undef SIGMA
