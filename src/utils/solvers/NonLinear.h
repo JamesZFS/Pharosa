e0f184163a51742b9800b766081d7aca1ee37378
郑逢时 <962907278@qq.com>
2019-05-15 12:15:03 +0800

    [update] combination of inheritance and template to accelarate

--- src/utils/solvers/NonLinear.h
+++ src/utils/solvers/NonLinear.h
@@ -19,7 +19,10 @@ namespace NonLinear
 	};
 
 	// x0, x1 both init values and output, 2-var function should implement (), d00(), d01, d10(), d11()
-	bool Solve2D(const BinFun &f0, const BinFun &f1, double &x0, double &x1, double tol = 1e-3);
+	template<class Fun0, class Fun1>
+	bool Solve2D(const Fun0 &f0, const Fun1 &f1, double &x0, double &x1, double tol = 1e-3);
 }
+
+#include "NonLinear.tcc"
 
 #endif //PHAROSA_NONLINEAR_H
